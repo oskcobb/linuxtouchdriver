2739bb90fbe2c5e825d22063e47d1f6c6ab8d8d7
Oskar Cobb <61062356+oskcobb@users.noreply.github.com>
2022-10-11 13:13:31 -0500

    updated main driver file with added comments

--- driver.py
+++ driver.py
@@ -1,31 +1,48 @@
+''' 
+Program by Oskar L. Cobb
+written on 10/10/2022 
+'''
+
+# import libraries
 import mouse
 import time
 from os import system, name
 import pyautogui
 
+# change time.sleep() to wait() because I kept typing it by mistake
 def wait(t):
     time.sleep(t)
 
+# setup clear screen funtion
 def cls():
-    #windows
+    # windows
     if name == 'nt':
         _ = system('cls')
-    #mac and linux
+    # mac and linux
     else:
         _ = system('clear')
 
+# clear
 cls()
 while True:
     try:
+        # check mouse possition
         i = mouse.get_position()
+        # display results
         print(i)
+        # duplicate values into another variable for reference
         i1 = i
+        # display results
         print(i1)
         wait(0.2)
+        # check if the varibles are the same
         if i == i1:
+            # release left mouse button
             pyautogui.mouseUp()
         else:
+            # press left mouse button
             pyautogui.mouseDown()
+    # if ctrl-c is pressed, exit cleanly
     except KeyboardInterrupt:
         cls()
         print("Exiting...")
