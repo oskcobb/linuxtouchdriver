d4e705e3dc4200d6e2c9cf50a252d3079479150f
Oskar Cobb <61062356+oskcobb@users.noreply.github.com>
2022-10-17 13:25:26 -0500

    update driver

--- driver.py
+++ driver.py
@@ -5,41 +5,37 @@ import pyautogui
 import time
 import math
 
-i = 5
-
 # change time.sleep() to wait() because I kept typing it by mistake
 def wait(t):
 	time.sleep(t)
 
+i = 5
+pyautogui.FAILSAFE = False
+
 while True:
-	try:
-		# check mouse possition
+	# check mouse possition
+	data = display.Display().screen().root.query_pointer()._data
+	x = data["root_x"]
+	y = data["root_y"]
+	# display results
+	print(x, y)
+   	# check if screen is being touched
+	if i == 5:
+		x1 = x
+		y1 = y
+		wait(0.1)
 		data = display.Display().screen().root.query_pointer()._data
 		x = data["root_x"]
 		y = data["root_y"]
 		# display results
 		print(x, y)
-		# start loop
-		if i = 5:
-			x1 = x
-			y1 = y
-			wait(0.5)
-			data = display.Display().screen().root.query_pointer()._data
-			x = data["root_x"]
-			y = data["root_y"]
-			# display results
-			print(x, y)
-			print(x1, y1)
-			# check if the mouse has moved
-			if x == x1 and y == y1:
-				# if it hasn't, then check if the previous position is close to the current one
-				while math.isclose(x, x1, rel_tol=500) and math.isclose(y, y1, rel_tol=500):
-					# hold left mouse button down
-					pyautogui.mouseDown()
-				else:
-					# release left mouse button
-					pyautogui.mouseUp()
-	# cleanly exit if user presses ctrl + C
-	except KeyboardInterrupt:
-		print(" \nExiting...")
-		exit()
+		print(x1, y1)
+		# check if the mouse has moved
+		if x == x1 and y == y1:
+			# if it hasn't, then check if the previous possition is close to the current one
+			while math.isclose(x, x1, rel_tol = 500) and math.isclose(y, y1, rel_tol = 500 ):
+				# hold left mouse button down
+				pyautogui.mouseDown()
+			else:
+				# release left mouse button
+				pyautogui.mouseUp()
